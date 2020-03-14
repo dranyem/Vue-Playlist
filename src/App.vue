5fb65c3104035b87b5fd32f649fc56673866c46c
dranyem <davidmeynard@yahoo.com.ph>
2020-03-14 13:59:15 -0600

    Added the two child components of the main parent

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SongList />
+    <PlayList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SongList from './components/SongList.vue'
+import PlayList from './components/PlayList.vue'
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
